a84f34a1a430b9c7a0b4e100a7ee8a19fb06b6d1
Jay Amrutkar <jayamrutkar.smart@gmail.com>
2019-09-13 22:02:58 +0530

    Fixing - Root resolver for query type 'Query' not found!
    Not working as per steps provided till https://www.howtographql.com/graphql-java/2-queries/. 
    
    [WARNING] /graphql
    javax.servlet.ServletException: javax.servlet.ServletException: com.coxautodev.graphql.tools.SchemaClassScannerError: Root resolver for query type 'Query' not found!
    	at org.eclipse.jetty.server.handler.HandlerCollection.handle(HandlerCollection.java:146)
    
    Caused by: javax.servlet.ServletException: com.coxautodev.graphql.tools.SchemaClassScannerError: Root resolver for query type 'Query' not found!
    	at org.eclipse.jetty.servlet.ServletContextHandler$Context.createServlet(ServletContextHandler.java:1332)
    	at org.eclipse.jetty.servlet.ServletHolder.newInstance(ServletHolder.java:1273)

--- src/main/java/com/howtographql/hackernews/Query.java
+++ src/main/java/com/howtographql/hackernews/Query.java
@@ -1,13 +1,13 @@
 package com.howtographql.hackernews;
 
-import com.coxautodev.graphql.tools.GraphQLRootResolver;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 
 import java.util.List;
 
 /**
  * Query root. Contains top-level queries.
  */
-class Query implements GraphQLRootResolver {
+class Query implements GraphQLQueryResolver {
 
     private final LinkRepository linkRepository;
 
